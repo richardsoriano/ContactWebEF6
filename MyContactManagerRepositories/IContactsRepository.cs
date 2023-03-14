4e1eca229c858adfbbfe4fc8a4451b179b92cb5f
richardsoriano <soriano.richard2020@gmail.com>
2023-03-13 20:51:25 -0700

    Added libraries for states and contacts services and repositories

---
+++ MyContactManagerRepositories/IContactsRepository.cs
@@ -0,0 +1,15 @@
+﻿using ContactWebModels;
+
+namespace MyContactManagerRepositories
+{
+    public interface IContactsRepository
+    {
+        Task<IList<Contact>> GetAllAsync();
+        Task<Contact?> GetAsync(int id);
+        Task<int> AddOrUpdateAsync(Contact contact);
+        Task<int> DeleteAsync(Contact contact);
+        Task<int> DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+
+    }
+}
